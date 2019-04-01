77b84998d997a1aea74dfd44a0d0ce7987a21267
田中敏之 <gakitherock19841102@gmail.com>
2019-04-01 23:06:50 +0900

    Markdownの導入

--- Gemfile
+++ Gemfile
@@ -18,6 +18,11 @@ gem 'bootstrap', '~> 4.1.1'
 gem 'jquery-rails'
 gem 'mini_racer'
 
+# markdown
+gem 'redcarpet', '~> 2.3.0'
+# シンタックスハイライト
+gem 'coderay'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/texts', to: 'texts#index'
 
+  root to: 'users#index'
 
 
 
