ad8e1a01e62eb716fb5aa4454ec81a03097e0e8b
tanaka takaharu <47471951+tanakin@users.noreply.github.com>
2019-05-13 00:16:12 +0900

    ジャンルでソートできるようにする

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,10 +2,18 @@ class TextsController < ApplicationController
 
     def index
         path_desc = request.fullpath.include?('desc')
-        @texts = Text.data_sort(path_desc)
-        @sort = Text.sort_key(path_desc)
+        path_genre = request.fullpath.include?('sort')
+
+        if !path_genre
+            @texts = Text.data_sort(path_desc)
+            @sort = Text.sort_key(path_desc)
+        else 
+            @texts = Text.genre_sort_method
+        end
+            
         @first_text = Text.first
-    end
+
+end
 
     def show
         @text = Text.find(params[:id])
--- app/models/text.rb
+++ app/models/text.rb
@@ -2,6 +2,7 @@ class Text < ApplicationRecord
     
     scope :asc_sort, ->{ order(id: :asc)}
     scope :desc_sort, ->{ order(id: :desc)}
+    scope :genre_sort, ->{ order(genre: :desc, id: :asc)}
 
     def self.data_sort(path)
         if path
@@ -17,6 +18,10 @@ class Text < ApplicationRecord
         else
             "desc"
         end
+    end
+
+    def self.genre_sort_method
+        Text.all.genre_sort
     end
 
 end
