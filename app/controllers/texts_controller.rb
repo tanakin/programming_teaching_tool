a36333990f98b7b59284bad92bec245156d558fc
tanaka takaharu <47471951+tanakin@users.noreply.github.com>
2019-05-12 23:17:01 +0900

    csvテストデータをインポート、texts/index.html.erbにてスキル欄のレベル番号の出力の仕方を調整

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,6 +4,7 @@ class TextsController < ApplicationController
         path_desc = request.fullpath.include?('desc')
         @texts = Text.data_sort(path_desc)
         @sort = Text.sort_key(path_desc)
+        @first_text = Text.first
     end
 
     def show
