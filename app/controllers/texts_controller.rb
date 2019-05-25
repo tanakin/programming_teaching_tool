1a75dcd3d54b9a6d85e3cb41dda0523a7f759ee9
tanaka takaharu <47471951+tanakin@users.noreply.github.com>
2019-05-25 23:41:17 +0900

    スキルボタンによるレコード並び替えメソッドをモデルへ実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,15 +1,13 @@
 class TextsController < ApplicationController
 
     def index
-        if request.fullpath.include?('desc')
-            @texts = Text.all.desc_sort
-            @sort = "asc"
-        else
-            @texts = Text.all.asc_sort
-            @sort = "desc"
-        end
+        
+        @texts = Text.data_sort(request)
+        @sort = Text.sort_key(request)
+
         @q = @texts.ransack(params[:q])
         @texts = @q.result
+
     end
 
     private
@@ -17,4 +15,5 @@ class TextsController < ApplicationController
     def text_params
         params.require(:text).permit(:genre, :title, :contents)
     end
+    
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,4 +1,5 @@
 class Text < ApplicationRecord
+  
   scope :asc_sort, ->{ order(id: :asc)}
   scope :desc_sort, ->{ order(id: :desc)}
 
@@ -9,4 +10,25 @@ class Text < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     []
   end
+
+  def self.data_sort(request)
+    if self.path_include_desc?(request)
+        Text.all.desc_sort
+    else
+        Text.all.asc_sort
+    end
+  end
+
+  def self.sort_key(request)
+    if self.path_include_desc?(request)
+        "asc"
+    else
+        "desc"
+    end
+  end
+
+  def self.path_include_desc?(path)
+    path.fullpath.include?('desc')
+  end
+  
 end
