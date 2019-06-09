25b2bee4b9fd31489074cbac327178b660928c19
tanaka takaharu <47471951+tanakin@users.noreply.github.com>
2019-06-09 20:44:06 +0900

    デプロイのためにroutes.rbとindex.html.erbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'texts', to: 'texts#index'
+  get '/texts', to: 'texts#index'
   root to: 'texts#index'
 
 
