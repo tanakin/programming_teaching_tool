1687b1a0a0b31523f52d6e0cf31e2b5066d161f6
tsuruki <sigeru0216@gmail.com>
2019-08-02 12:34:25 +0900

    testの修正

--- spec/models/import_csv_spec.rb
+++ spec/models/import_csv_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe ImportCsv, type: :model do
   describe "#import" do
     let(:csv) { "spec/fixtures/csv_test.csv" }
+    let(:csv_test) { Text.find_by(title: 'タイトル1') }
 
     before do
       ImportCsv.import(csv)
@@ -10,6 +11,18 @@ RSpec.describe ImportCsv, type: :model do
 
     it "csvデータ件数とがTextモデルにインポートされた件数が等しいこと" do
       expect(Text.count).to eq 5
+    end
+
+    it "titleの値が取得できること" do
+      expect(csv_test.title).to eq "タイトル1"
+    end
+    
+    it "contentsの値が取得できること" do
+      expect(csv_test.contents).to eq "コンテンツ1"
+    end
+
+    it "genreの値が取得できること" do
+      expect(csv_test.genre).to eq "ジャンル1"
     end
   end
   
